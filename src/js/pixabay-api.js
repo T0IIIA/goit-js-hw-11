2791e529b83a0e5017b3e1c19a4d51cd7a49dab3
Anton Gatsko <89578801+T0IIIA@users.noreply.github.com>
2024-05-30 14:51:05 +0300

    js hw-11 v.1.1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,11 +1,5 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css"
-import { imageTamplate } from './render-functions.js';
 export { getImages }
 
-const imageGallery = document.querySelector('.gallery');
 //----------------------------------
 
 function getImages(imageName) {
@@ -27,32 +21,9 @@ function getImages(imageName) {
       }
       return res.json();
     })
-    .then(data => {
-      if (data.hits.length === 0) {
-      imageGallery.innerHTML = '';
-      return iziToast.error({ ...errorParams });
-      }
-
-      const markup = imageTamplate(data.hits);
-      imageGallery.innerHTML = markup;
-      lightbox.refresh();
-    })
-    .catch(error => console.log(error))
 }
 
-//------------------iziToast params----------------
 
-const errorParams = {
-  position: 'topRight',
-  timeout: 1800,
-  maxWidth: 300,
-  icon: 'none',
-  message: 'Sorry, there are no images matching your search query. Please try again!',
-}
-
-//------------------Simple Light box----------------
-
-let lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250, captionsData: "alt" });
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css"
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 //----------------------------------
 import { getImages } from './js/pixabay-api.js';
+import { imageTamplate } from './js/render-functions.js';
+
 const searchForm = document.querySelector('.search-form');
+const imageGallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
 
 //----------------------------------
@@ -24,6 +29,17 @@ function onSubmit(event) {
   }
 
   getImages(imageName)
+  .then(data => {
+      if (data.hits.length === 0) {
+      imageGallery.innerHTML = '';
+      return iziToast.error({ ...errorParams });
+      }
+
+      const markup = imageTamplate(data.hits);
+      imageGallery.innerHTML = markup;
+      lightbox.refresh();
+    })
+    .catch(error => console.log(error))
     .finally(() => {
       hideLoader();
     });
@@ -39,3 +55,17 @@ function showLoader() {
 function hideLoader() {
   loader.style.display = 'none';
 }
+
+//------------------iziToast params----------------
+
+const errorParams = {
+  position: 'topRight',
+  timeout: 1800,
+  maxWidth: 300,
+  icon: 'none',
+  message: 'Sorry, there are no images matching your search query. Please try again!',
+}
+
+//------------------Simple Light box----------------
+
+let lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250, captionsData: "alt" });
